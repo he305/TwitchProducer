0b69e181a4fae17bc6c468f04f1be64ac4ad1c62
Sergey <he305@mail.ru>
2022-05-28 22:15:54 +0300

    Added streamer service

--- src/main/java/io/github/he305/TwitchProducer/common/interfaces/StreamerService.java
+++ src/main/java/io/github/he305/TwitchProducer/common/interfaces/StreamerService.java
@@ -1,2 +1,13 @@
-package io.github.he305.TwitchProducer.common.interfaces;public interface StreamerService {
+package io.github.he305.TwitchProducer.common.interfaces;
+
+import io.github.he305.TwitchProducer.common.entities.Streamer;
+import lombok.NonNull;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface StreamerService {
+    List<Streamer> getAllStreamers();
+    Optional<Streamer> getStreamerByName(@NonNull String nickname);
+    Streamer addStreamer(@NonNull Streamer streamer);
 }
