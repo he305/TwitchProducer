397eb636480deac2eb068f5b0f2d234ff9f466c8
Sergey <he305@mail.ru>
2022-05-28 22:13:15 +0300

    Added model mapper bean and StreamerBodyDto

--- src/main/java/io/github/he305/TwitchProducer/application/dto/ModelMapperConfiguration.java
+++ src/main/java/io/github/he305/TwitchProducer/application/dto/ModelMapperConfiguration.java
@@ -1,2 +1,13 @@
-package io.github.he305.TwitchProducer.application.dto;public class ModelMapperConfiguration {
+package io.github.he305.TwitchProducer.application.dto;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfiguration {
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
 }
--- src/main/java/io/github/he305/TwitchProducer/application/dto/StreamerBodyDto.java
+++ src/main/java/io/github/he305/TwitchProducer/application/dto/StreamerBodyDto.java
@@ -1,5 +1,12 @@
 package io.github.he305.TwitchProducer.application.dto;
 
-public class StreamerBodyRequest {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class StreamerBodyDto {
     private String nickname;
 }
