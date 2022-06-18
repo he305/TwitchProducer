9f38137507d5edb26c3c921ba5ef1f6601dd7638
sergey <he305@mail.ru>
2022-06-18 17:42:00 +0300

    Added core PersonDto

---
+++ src/main/java/com/github/he305/twitchproducer/common/dto/PersonDto.java
@@ -0,0 +1,13 @@
+package com.github.he305.twitchproducer.common.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+public class PersonDto {
+    private String firstName;
+    private String lastName;
+}
