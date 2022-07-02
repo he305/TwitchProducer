8901dd0b6290268ee298d29c79d14601af137a4e
sergey <he305@mail.ru>
2022-07-02 20:20:59 +0300

    Added Repository annotation

--- src/main/java/com/github/he305/twitchproducer/application/repositories/PersonRepository.java
+++ src/main/java/com/github/he305/twitchproducer/application/repositories/PersonRepository.java
@@ -2,9 +2,11 @@ package com.github.he305.twitchproducer.application.repositories;
 
 import com.github.he305.twitchproducer.common.entities.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface PersonRepository extends JpaRepository<Person, Long> {
     Optional<Person> findByLastName(String lastName);
 }
--- src/main/java/com/github/he305/twitchproducer/application/repositories/StreamRepository.java
+++ src/main/java/com/github/he305/twitchproducer/application/repositories/StreamRepository.java
@@ -2,6 +2,8 @@ package com.github.he305.twitchproducer.application.repositories;
 
 import com.github.he305.twitchproducer.common.entities.Stream;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface StreamRepository extends JpaRepository<Stream, Long> {
 }
