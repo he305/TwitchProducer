8a3fae0401519ba8e752193ae386d0cbe85eed4f
Sergey <he305@mail.ru>
2022-07-02 21:34:01 +0300

    Added swagger

--- src/main/java/com/github/he305/twitchproducer/TwitchProducerApplication.java
+++ src/main/java/com/github/he305/twitchproducer/TwitchProducerApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableTransactionManagement
 @EnableJpaAuditing
+@EnableSwagger2
 public class TwitchProducerApplication {
 
     public static void main(String[] args) {
